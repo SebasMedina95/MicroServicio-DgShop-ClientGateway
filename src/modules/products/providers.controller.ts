9cc4acbd1716e1b4e12ed9b758c973166ac9ce23
Sebastian Medina <jsebastian19952011@gmail.com>
2024-06-28 00:17:36 -0500

    Se implementa el apartado de proveedores del micro servicio de productos.

--- src/modules/products/providers.controller.ts
+++ src/modules/products/providers.controller.ts
@@ -1,8 +1,100 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller,
+         Delete,
+         Get,
+         Inject, 
+         Param, 
+         Patch, 
+         Post} from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError } from 'rxjs';
+
+import { PRODUCT_SERVICE } from '../../config/services';
+
+import { CreateProviderDto } from '../../validators/products/providers-dto/create-provider.dto';
+import { UpdateProviderDto } from '../../validators/products/providers-dto/update-provider.dto';
+import { PageOptionsDto } from '../../helpers/paginations/dto/page-options.dto';
 
 @Controller('providers')
 export class ProvidersController {
-  constructor() {}
+  
+  constructor(
+    @Inject(PRODUCT_SERVICE) private readonly productsClient: ClientProxy,
+  ) {}
+
+  @Post('/create')
+  async createProvider(
+    @Body() createProviderDto: CreateProviderDto
+  ){
+    
+    return this.productsClient.send({ cmd: 'create_provider' }, createProviderDto )
+      .pipe(
+        catchError(err => { throw new RpcException(err) })
+      )
+    
+  }
+
+  @Get('/get-paginated')
+  async getAllProviders(
+    @Body() pageOptionsDto: PageOptionsDto
+  ){
+    //Lo que tenemos dentro del send, el primer argumento es el nombre que le dimos en el @MessagePattern
+    //que en este caso fue { cmd: 'get_providers_paginated' }, y el segundo es el Payload, es decir, el cuerpo
+    //de la petición para enviar los parámetros
+    return this.productsClient.send({ cmd: 'get_providers_paginated' }, pageOptionsDto )
+      .pipe(
+        catchError(err => { throw new RpcException(err) })
+      )
+  }
+
+  @Get('/get-by-id/:id')
+  async getProviderById(
+    @Param('id') id: number
+  ){
+
+    try {
+
+      return this.productsClient.send({ cmd: 'get_provider_by_id' }, { 
+        id 
+      }).pipe(
+          catchError(err => { throw new RpcException(err) })
+        )
+
+    } catch (error) {
+
+      throw new RpcException(error);
+
+    } 
+    
+  }
+
+  @Patch('/update/:id')
+  async updateProvider(
+    @Param('id') id: number,
+    @Body() updateProviderDto: UpdateProviderDto
+  ){
+    
+    return this.productsClient.send({ cmd: 'update_provider' }, {
+      id,
+      ...updateProviderDto
+    }).pipe(
+      catchError(err => { throw new RpcException(err) })
+    )
+    
+    
+  }
+
+  @Delete('/delete/:id')
+  async deleteProvider(
+    @Param('id') id: number
+  ){
+    
+    return this.productsClient.send({ cmd: 'remove_logic_provider' }, {
+      id
+    }).pipe(
+      catchError(err => { throw new RpcException(err) })
+    )
+    
+  }
 
   
 
