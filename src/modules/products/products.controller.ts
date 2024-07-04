d53c482ae4921b4831cd62ce47979da79df41ee6
Sebastian Medina <jsebastian19952011@gmail.com>
2024-07-03 22:41:07 -0500

    Se implementa la conexión al listar y obtener por ID del MS de Productos

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -1,14 +1,17 @@
 import { Body,
          Controller,
+         Get,
          Inject,
+         Param,
          Post,
          UploadedFiles,
          UseInterceptors } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { FilesInterceptor } from '@nestjs/platform-express';
+import { catchError } from 'rxjs';
 import { PRODUCT_SERVICE } from '../../config/services';
 import { CreateProductDto } from '../../validators/products/products-dto/create-product.dto';
-import { catchError } from 'rxjs';
+import { PageOptionsDto } from '../../helpers/paginations/dto/page-options.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -34,6 +37,40 @@ export class ProductsController {
       ).pipe(
         catchError(err => { throw new RpcException(err) })
       )
+    
+  }
+
+  @Get('/get-paginated')
+  async getAllProducts(
+    @Body() pageOptionsDto: PageOptionsDto
+  ){
+    //Lo que tenemos dentro del send, el primer argumento es el nombre que le dimos en el @MessagePattern
+    //que en este caso fue { cmd: 'get_products_paginated' }, y el segundo es el Payload, es decir, el cuerpo
+    //de la petición para enviar los parámetros
+    return this.productsClient.send({ cmd: 'get_products_paginated' }, pageOptionsDto )
+      .pipe(
+        catchError(err => { throw new RpcException(err) })
+      )
+  }
+
+  @Get('/get-by-id/:id')
+  async getProductsById(
+    @Param('id') id: number
+  ){
+
+    try {
+
+      return this.productsClient.send({ cmd: 'get_product_by_id' }, { 
+        id 
+      }).pipe(
+          catchError(err => { throw new RpcException(err) })
+        )
+
+    } catch (error) {
+
+      throw new RpcException(error);
+
+    } 
     
   }
 
