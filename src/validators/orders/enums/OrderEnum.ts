c09411110d62169887b87b87f850b703e38f3d11
Sebastian Medina <jsebastian19952011@gmail.com>
2024-07-21 23:05:24 -0500

    Se ajusta el DTO y la estrucutra de recepción para el registro de una orden de compra

---
+++ src/validators/orders/enums/OrderEnum.ts
@@ -0,0 +1,15 @@
+
+export enum OrderStatus {
+    PENDIENTE =  "PENDIENTE",
+    ENTREGADA =  "ENTREGADA",
+    SOLICITADA =  "SOLICITADA",
+    CANCELADA =  "CANCELADA",
+}
+
+
+export const OrderStatusList = [
+    OrderStatus.PENDIENTE,
+    OrderStatus.ENTREGADA,
+    OrderStatus.SOLICITADA,
+    OrderStatus.CANCELADA,
+]
